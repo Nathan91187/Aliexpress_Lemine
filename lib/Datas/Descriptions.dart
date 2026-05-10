994dc1dcea255a91cba3910fb271606f52aa807f
Nathan91187 <natanasnake91@gmail.com>
2026-05-10 23:06:35 +0300

    added item description storage map

---
+++ lib/Datas/Descriptions.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+Map <String, String> myDescriptions = {
+  'Comfortable modern bed for restful sleep': 'Transform your bedroom with this modern bed designed to deliver both comfort and durability. Crafted with a strong frame and a stylish finish, it provides reliable support for a restful night’s sleep while complementing any bedroom setup. Its minimalist design blends easily with modern interiors, making it a perfect choice for everyday comfort and long-term use.',
+};
