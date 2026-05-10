f9ad240f85902ec27a09098769a97cdada942531
Nathan91187 <natanasnake91@gmail.com>
2026-05-10 23:17:41 +0300

    Added purchasing option buttons

---
+++ lib/Widgets/PurchaseOptions.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+class Purchaseoptions extends StatelessWidget {
+  final String info;
+  final Color color,textColor;
+  const Purchaseoptions({super.key, required this.info, required this.color, required this.textColor});
+
+  @override
+  Widget build(BuildContext context) {
+    final myWidth = MediaQuery.of(context).size.height;
+    return InkWell(
+      onTap: (){
+      },
+      child: Container(
+        height: 0.05 * myWidth,
+        color: color,
+        child: Center(
+          child: Text(
+            info,
+            style: TextStyle(
+              fontSize: 0.017 * myWidth,
+              fontFamily: "Inter",
+              fontWeight: FontWeight.w900,
+              color: textColor
+            )
+          ),
+        ),
+      ),
+    );
+
+  }
+}
